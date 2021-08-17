bbafde36179185276cf832cea2cca39bbd27de04
kgs <kgs@gmail.com>
2021-08-17 17:16:00 +0900

    +++

--- resources/views/decision/received_report.blade.php
+++ resources/views/decision/received_report.blade.php
@@ -91,7 +91,7 @@
                                 <th style="width: 7%;">{{ trans('decideManage.table.shipName') }}</th>
                                 <th style="width: 7%;">{{ trans('decideManage.table.voy_no') }}</th>
                                 <th style="width: 7%;" class="for-pc">{!! trans('decideManage.table.profit_type') !!}</th>
-                                <th style="width: 25%;" class="for-pc">{{ trans('decideManage.table.content') }}</th>
+                                <th style="width: 25%; max-width: 600px; overflow: hidden;text-overflow: ellipsis;" class="for-pc">{{ trans('decideManage.table.content') }}</th>
                                 <th style="width: 5%;" class="for-pc">{{ trans('decideManage.table.currency') }}</th>
                                 <th style="width: 10%;" class="for-pc">{{ trans('decideManage.table.amount') }}</th>
                                 <th style="width: 5%;">{{ trans('decideManage.table.reporter') }}</th>
