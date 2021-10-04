a7b9ae27beba80c3fe8a73c86fd577fd8b36b507
kgs <kgs@gmail.com>
2021-10-04 19:18:48 +0900

    +++

--- resources/views/decision/received_report.blade.php
+++ resources/views/decision/received_report.blade.php
@@ -92,7 +92,7 @@
                                     <th style="width: 7%;">{!! trans('decideManage.table.profit_type') !!}</th>
                                     <th style="width: 25%;" class="for-pc">{{ trans('decideManage.table.content') }}</th>
                                     <th style="width: 5%;" class="for-pc">{{ trans('decideManage.table.currency') }}</th>
-                                    <th style="width: 10%;">{{ trans('decideManage.table.amount') }}</th>
+                                    <th style="width: 10%;" class="text-center">{{ trans('decideManage.table.amount') }}</th>
                                     <th style="width: 5%;">{{ trans('decideManage.table.reporter') }}</th>
                                     <th style="width: 5%;" class="for-pc">涉及<br>部门</th>
                                     <th style="width: 5%;" class="for-pc">{!! trans('decideManage.table.attachment') !!}</th>
